6b5c59bf0d203e9f9f38b4fcb14e1c51758d0fd6
caballerosindy11@gmail.com <caballerosindy11@gmail.com>
2022-10-03 12:34:28 -0500

    elimine tiburon de la lista

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,7 +3,8 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
+        miVertebrados[1] = new Vertebrado("perro", "Mamifero");
+
         
         return miVertebrados;
     }
